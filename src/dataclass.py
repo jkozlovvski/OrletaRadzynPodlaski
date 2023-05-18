2fc3dcf79a74b2e085c6699857c91eba78aca133
Julian Kozlowski <julian.kozlowski99@gmail.com>
2023-05-18 17:17:10 +0200

    Adding RGB option

--- src/dataclass.py
+++ src/dataclass.py
@@ -6,15 +6,29 @@ from PIL import Image
 import numpy as np
 import random
 
+from sklearn.feature_extraction.text import TfidfVectorizer
 from utils import id2label, label2id
 
 
 class TextDataSet(Dataset):
+    def _clean_text(self, text):
+        text = text.lower()  # making text lowercase
+        return text
+
     def __init__(self, dataset_path, labels_path):
         self.texts = pd.read_pickle(dataset_path)
         self.labels = pd.read_pickle(labels_path)
         self.labels = {k: id2label[v] for k, v in self.labels.items()}
         self.images = list(self.texts.keys())
+
+        # preprocessing text
+        self.texts = {k: self._clean_text(v) for k, v in self.texts.items()}
+        tfidf_vectorizer = TfidfVectorizer(stop_words="english")
+        tfidf_vectorizer.fit(list(self.texts.values()))
+        self.vectorizer = tfidf_vectorizer
+        self.texts = {k: tfidf_vectorizer.transform([v]) for k, v in self.texts.items()}
+
+        # todo: check for classes distributions on words
 
     def __len__(self):
         return len(self.texts)
@@ -37,9 +51,13 @@ class ImageDataSet(Dataset):
                 self.labels[file_name] = id2label[label]
         self.transform = transform
         self.images = list(self.labels.keys())
+        self.use_rgb = False
 
     def __len__(self):
         return len(self.labels)
+
+    def use_rgb(self):
+        self.use_rgb = True
 
     def __getitem__(self, idx):
         image_name = self.images[idx]
@@ -47,7 +65,8 @@ class ImageDataSet(Dataset):
             self.img_dir, label2id[self.labels[image_name]], image_name
         )
         image = Image.open(img_path)
-        #       image.show()
+        if self.use_rgb:
+            image = image.convert("RGB")
         return np.array(image), self.labels[image_name]
 
 
@@ -56,6 +75,7 @@ text_dataset_train = TextDataSet(
 )
 image_dataset_train = ImageDataSet("../datasets/train_set")
 
+
 if __name__ == "__main__":
     print("Number of examples in text dataset: ", len(text_dataset_train))
     print(
@@ -63,8 +83,7 @@ if __name__ == "__main__":
         text_dataset_train[random.randint(0, len(text_dataset_train))],
     )
 
-    print("Number of examples in image dataset: ", len(image_dataset_train))
-    print(
-        "Some random example",
-        image_dataset_train[random.randint(0, len(text_dataset_train))],
-    )
+    # print(
+    #     "Some random example",
+    #     image_dataset_train[random.randint(0, len(text_dataset_train))],
+    # )
